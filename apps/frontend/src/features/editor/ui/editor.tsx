abc6fed0d25c7a4101b2958863ed16c97c7b7162
SuperOleg39 <surf4sites@yandex.ru>
2022-07-10 14:52:27 +0000

    chore: try gitpod

--- apps/frontend/src/features/editor/ui/editor.tsx
+++ apps/frontend/src/features/editor/ui/editor.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useStore } from 'effector-react';
+import { $blocks } from '../../../entities/block/model/block';
 import { Document } from '../../../entities/document/model/document';
 import { Line } from './line';
 
 export const Editor = ({ document }: { document: Document }) => {
-    return (
-        <>
-            <Line type="title" content={document.title} />
+	const blocks = useStore($blocks);
 
-            {document.blocks.map((block) => (
-                <Line type={block.type} content={block.content} />
-            ))}
-        </>
-    );
+	return (
+		<>
+			<Line
+				block={{ id: 'title', type: 'title', content: document.title }}
+			/>
+
+			{document.blocks.map((block) =>
+				blocks[block] ? <Line key={blocks[block].id} block={blocks[block]} /> : false
+			)}
+		</>
+	);
 };
--- apps/frontend/src/features/editor/ui/line.tsx
+++ apps/frontend/src/features/editor/ui/line.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Block } from '../../../entities/block/model/block';
 
-export const Line = (block: Block) => {
-    switch (block.type) {
-        case 'title': {
-            return <h1 contentEditable>{block.content}</h1>;
-        }
-        case 'text': {
-            return <div contentEditable>{block.content}</div>;
-        }
-        default: {
-            return null;
-        }
-    }
+export const Line = ({ block }: { block: Block }) => {
+	switch (block.type) {
+		case 'title': {
+			return (
+				<h1
+					contentEditable
+					dangerouslySetInnerHTML={{ __html: block.content }}
+				/>
+			);
+		}
+		case 'text': {
+			return (
+				<div
+					contentEditable
+					dangerouslySetInnerHTML={{ __html: block.content }}
+				/>
+			);
+		}
+		default: {
+			return null;
+		}
+	}
 };

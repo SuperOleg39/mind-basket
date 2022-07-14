210777bb86d1f72b12dc2787730c8ff9f5c2f8c8
SuperOleg39 <surf4sites@yandex.ru>
2022-07-14 11:47:28 +0000

    build: fix hmr in gitpod

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
-// import reactRefresh from '@vitejs/plugin-react-refresh';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react({ babel: { babelrc: true } })],
+    server: {
+        hmr: process.env.GITPOD_WORKSPACE_URL
+        ? {
+            // removes the protocol and replaces it with the port we're connecting to
+            host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+            protocol: 'wss',
+            clientPort: 443
+        }
+        : true
+    }
 });
